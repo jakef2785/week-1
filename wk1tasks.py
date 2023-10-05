f7d40bc2ddc4e215bc5115b67396f5a128d9c631
Ferreira <J.Ferreira2548@student.leedsbeckett.ac.uk>
2023-10-05 12:51:41 +0100

    week 1 task script

---
+++ wk1tasks.py
@@ -0,0 +1,66 @@
+'''
+total = 0
+
+total -= 1
+print("The total is", total)
+
+total = total * 4
+print("The total is", total)
+
+total = total / 2
+print("The total is", total)
+
+
+total = 98.2
+count = 5
+average = total/count
+print(average)
+
+
+"ABC" * 10
+
+
+age = input ("enter your age")
+age = int(age)
+print ("in one year your age will be", age + 1)
+
+
+
+print("Hi, \nWhat is your name ")
+name = input()
+print("a \\ b\tc \' d \"")
+
+
+print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\nHello world\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+
+'''
+
+print("Name")
+name = input()
+print("Hello ", name," how are you")
+
+
+cTemp = input("Temperature")
+cTemp = int(cTemp)
+fTemp = (cTemp*9 / 5)+32
+
+print("Fahrenheit temperature is ", fTemp)
+
+
+noStudents = int(input("How many students?"))
+groupSize = int(input("How large are the groups"))
+numberOfGroups = noStudents/groupSize
+remainingStudents = noStudents%groupSize
+print("There are ", numberOfGroups, "Groups \n with", remainingStudents, "students left over")
+
+
+totalStudents = int(input("How many students"))
+totalSweets = int(input("How many sweets"))
+sweetsPerPupil = totalSweets/totalStudents
+remainingSweets = totalSweets%totalStudents
+print("Give each pupil ", sweetsPerPupil, " sweets \n and you will have ", remainingSweets, " left over")
+
+
+
+
+
